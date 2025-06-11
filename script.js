231644be7d594e0442f13776659444ee9fefb771
Gabrielle De Castro Santos Catarino <catarinogabrielle@gmail.com>
2025-06-11 17:53:24 -0300

    at45

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ async function sendMessage() {
     input.value = "";
 
     try {
-        const response = await fetch('https://chatboat-catarinos.vercel.app/', {
+        const response = await fetch('https://chatboat-catarinos.vercel.app/chat', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
